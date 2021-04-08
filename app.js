4e13648f78dd21de18453162571ad2b6460985a3
saulzh <z@ruiruizhang.com>
2021-04-09 00:18:15 +0800

    add rm action before copy

--- app.js
+++ app.js
@@ -5,14 +5,21 @@ const shell = require('shelljs');
 let type = process.argv[2];
 
 if (type === 'react') {
+    shell.rm('-rf', '../saulzh.github.io/dist/react');
     shell.cp('-R', path.resolve(__dirname, '../template-react/build'), path.resolve(__dirname, '../saulzh.github.io/dist/react'));
     console.log('react move success');
 } else if (type === 'vue') {
+    shell.rm('-rf', '../saulzh.github.io/dist/vue');
     shell.cp('-R', path.resolve(__dirname, '../template-vue/dist'), path.resolve(__dirname, '../saulzh.github.io/dist/vue'));
     console.log('vue move success');
 } else if (type === 'vue3') {
+    shell.rm('-rf', '../saulzh.github.io/dist/vue3');
     shell.cp('-R', path.resolve(__dirname, '../template-vue3/dist'), path.resolve(__dirname, '../saulzh.github.io/dist/vue3'));
     console.log('vue3 move success');
+} else if (type === 'flutter') {
+    shell.rm('-rf', '../saulzh.github.io/dist/flutter');
+    shell.cp('-R', path.resolve(__dirname, '../../flutter/flutter_collection/build/web'), path.resolve(__dirname, '../saulzh.github.io/dist/flutter'));
+    console.log('flutter move success');
 }
 
 
